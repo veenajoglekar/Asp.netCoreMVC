357643905acb0ae1b5dfd7da56160cdf990201cd
veenajoglekar <veena1695@gmail.com>
2021-09-01 16:19:52 +0530

    NewProjectCreated,IdentityCreated

---
+++ EmployeeManagementSys/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EmployeeManagementSys.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EmployeeManagementSys.Areas.Identity.IdentityHostingStartup))]
+namespace EmployeeManagementSys.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
