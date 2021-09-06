64d998f1f82128efdc17d5bf8b41cbcf0501455d
veenajoglekar <veena1695@gmail.com>
2021-09-06 23:57:24 +0530

    ValidationAdded

--- EmployeeManagementSys.DAL/Data/Model/Employee.cs
+++ EmployeeManagementSys.DAL/Data/Model/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,18 @@ namespace EmployeeManagementSys.DAL.Data.Model
     {
         public int EmployeeId { get; set; }
 
+        [Required(ErrorMessage = "Please enter first name")]
+        [StringLength(100)]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Please enter last name")]
+        [StringLength(100)]
         public string LastName { get; set; }
 
+
+        [Required(ErrorMessage = "Please enter email address")]
+        [Display(Name = "Email Address")]
+        [EmailAddress]
         public string Email { get; set; }
 
         public int Salary{ get; set; }
